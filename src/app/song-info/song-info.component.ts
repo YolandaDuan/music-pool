def47608079f343a89ecbea247b48af1f16c8134
Yolanda Duan <malmsduan@gmail.com>
2021-09-27 00:50:52 +0200

    make id of song be routable

--- src/app/song-info/song-info.component.ts
+++ src/app/song-info/song-info.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common'; 
+
 import { Song } from '../song';
+import { SongService } from '../song.service';
 
 @Component({
   selector: 'app-song-info',
@@ -7,11 +11,25 @@ import { Song } from '../song';
   styleUrls: ['./song-info.component.css']
 })
 export class SongInfoComponent implements OnInit {
-  @Input() song?: Song;
+  song: Song | undefined;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private songService: SongService,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+    this.getSong();
   }
 
+  getSong(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.songService.getSong(id)
+      .subscribe(song => this.song = song)
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
--- src/app/song.service.ts
+++ src/app/song.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { Song } from './song';
 import { SONGS } from './mock-songs';
-import { Observable, of } from 'rxjs';
 import { MessagesService } from './messages.service';
 
 @Injectable({
@@ -15,5 +17,11 @@ export class SongService {
     const songs = of(SONGS);
     this.messageService.add('SongService: fetched songs successfully');
     return songs;
+  }
+
+  getSong(id: number): Observable<Song> {
+    const song = SONGS.find(h => h.id === id)!;
+    this.messageService.add(`SongService: fetched song with id = ${id}`);
+    return of(song);
   }
 }
