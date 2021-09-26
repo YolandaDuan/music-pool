7bc115534fd95e075e0357b9bd9c6ea86d58f23e
Yolanda Duan <malmsduan@gmail.com>
2021-09-26 20:33:18 +0200

    add routers for songs component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SongsComponent } from './songs/songs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'songs', component: SongsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
